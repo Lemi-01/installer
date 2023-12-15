1f91a63871b678f0df7c794e9e2a64f22b18aabf
Lemi-01 <98029196+Lemi-01@users.noreply.github.com>
2023-12-15 15:46:28 +0200

    Update installer.sh
    added signal

--- installer.sh
+++ installer.sh
@@ -33,7 +33,7 @@ one(){
 	echo "discord, betterdiscordctl, brave-bin, mullvad-vpn,"
 	echo "opentabletdriver, osu-lazer, prismlauncher, protonup-qt,"
 	echo "protontricks, qbittorrent, ttf-firacode-nerd, steam,"
-	echo "btop, lutris, winetricks,"
+	echo "btop, lutris, winetricks, signal"
 	echo "Wine Dependecy Hell (Do Your Own Research)"
 	pause
         yay -Syyu neofetch lolcat cowsay appimagelauncher discord betterdiscordctl brave-bin mullvad-vpn opentabletdriver osu-lazer prismlauncher protonup-qt protontricks qbittorrent ttf-firacode-nerd steam btop lutris winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox --noconfirm --sudoloop
@@ -146,6 +146,12 @@ one(){
 	echo " $package"
 	fi
 	package=winetricks
+	if pacman -Qs $package > /dev/null ; then
+	echo " $package"
+	else
+	echo " $package"
+	fi
+	package=signal-desktop
 	if pacman -Qs $package > /dev/null ; then
 	echo " $package"
 	else
